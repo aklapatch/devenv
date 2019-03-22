ac40c3d2302a60d5d5a731a31685cd6d6c745446
Aaron Klapatch <klapatchaaron@gmail.com>
2019-03-22 18:42:25 -0400

    Add files via upload

--- mingw64.bat
+++ mingw64.bat
@@ -5,13 +5,11 @@ set "VSCODEDIR=%~dp0vscodium\"
 set "MSYSDir=%~dp0msys64\"
 set "AuxDir=%~dp0grabber\root\"
 
-set "ToolsDirs=%MSYSDir%mingw64\bin;%MSYSDir%usr\bin;%AuxDir%;%AuxDir%bin"
+set "ToolsDirs=%AuxDir%;%AuxDir%bin"
 
 :: extend the path to include an auxiliary location (msys and tools dir)
 set "PATH=%ToolsDirs%;%PATH%"
 
-:: change extension dir and settings dir
+:: start msys window with extended path and mingw window
 cd %~dp0
-start %VSCODEDIR%VSCodium.exe --user-data-dir "%VSCODEDIR%settings" --extensions-dir "%VSCODEDIR%extensions"
-
-goto :eof
+%MSYSDir%msys2_shell.cmd -here -full-path -mingw64

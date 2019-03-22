1e1e626091c8205ad93e20a3fc4efb9d34082128
Aaron Klapatch <klapatchaaron@gmail.com>
2019-03-22 18:19:41 -0400

    Update vscodium.bat

--- vscodium.bat
+++ vscodium.bat
@@ -1,11 +1,11 @@
-::@echo off
+@echo off
 SETLOCAL EnableDelayedExpansion
 
 set "VSCODEDIR=%~dp0vscodium\"
 set "MSYSDir=%~dp0msys64\"
 set "AuxDir=%~dp0grabber\root\"
 
-set "ToolsDirs=%MSYSDir%mingw64\bin;%MSYSDir%usr\bin;%AuxDir%;%AuxDir%bin
+set "ToolsDirs=%MSYSDir%mingw64\bin;%MSYSDir%usr\bin;%AuxDir%;%AuxDir%bin"
 
 :: extend the path to include an auxiliary location (msys and tools dir)
 set "PATH=%ToolsDirs%;%PATH%"
